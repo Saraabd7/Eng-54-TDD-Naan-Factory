9cb03ed757a04ddd6f955252ed643cf46973e0d4
Sara Abdrabu <Sabdrabu@tcadmins-air.home>
2020-03-05 21:14:20 +0000

    Updates

--- Naan_factory_functions.py
+++ Naan_factory_functions.py
@@ -1,6 +1,6 @@
 # Factory functions
 # define our function  here
-from run_naan_factory import *
+from Run_Naan_Factory import *
 
 def make_dough(arg1, arg2):
     # if argument is water and argument 2 is flour
@@ -8,3 +8,20 @@ def make_dough(arg1, arg2):
     # else return
         # not dough
     return 'dough'
+
+def make_dough(ingredient1,ingredient2):
+    if (ingredient1 == 'water') and (ingredient2 == 'flour'):
+        return 'dough'
+    elif (ingredient1 == 'flour') and (ingredient2 == 'water'):
+        return 'dough'
+    else:
+        return 'Not dough'
+
+def bake_dough(ingredient):
+    if ingredient == 'dough':
+        return 'naan'
+    else:
+        return 'not naan'
+
+def run_factory(ing1,ing2):
+    return bake_dough(make_dough(ing1, ing2))
--- Test_Naan_Factory.py
+++ Test_Naan_Factory.py
@@ -1,14 +1,32 @@
 # import naan factory functions
 # define and run tests
+#
+# from Naan_factory_functions import *
+#
+# #1
+# # As a user, I can use the make_dough with water and flour to make dough.
+# print("calling make_dough with water and flour, expect 'dough' as a result")
+# print(make_dough('water', 'flour') == 'dough')
+# print('got:', make_dough('water', 'flour'))
+#
+# print("calling make_dough with water and cement, expect 'not dough' as a result")
+# print(make_dough('water', 'cement') == 'not dough')
+# print('got:', make_dough('water', 'cement'))
 
-from naan_factory_functions import *
-
-#1
+# Exercise:
+# 1
 # As a user, I can use the make_dough with water and flour to make dough.
-print("calling make_dough with water and flour, expect 'dough' as a result")
-print(make_dough('water', 'flour') == 'dough')
-print('got:', make_dough('water', 'flour'))
 
-print("calling make_dough with water and cement, expect 'not dough' as a result")
-print(make_dough('water', 'cement') == 'not dough')
-print('got:', make_dough('water', 'cement'))
+# 2
+# As a user, I can use the bake_dough with dough to get naan.
+
+# 3
+# As a user, I can user the run_factory with water and flour and get naan
+
+from Naan_factory_functions import *
+
+print(make_dough('water', 'flour') == 'dough')
+
+print(bake_dough('dough') == 'naan')
+
+print(run_factory('water', 'flour') == 'naan')
