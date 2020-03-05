03ec936891f90e4fa5b16a09746623a678bbff17
Sara Abdrabu <Sabdrabu@tcadmins-MacBook-Air.local>
2020-03-05 17:19:36 +0000

    TDD

---
+++ Naan_factory_functions.py
@@ -0,0 +1,10 @@
+# Factory functions
+# define our function  here
+from run_naan_factory import *
+
+def make_dough(arg1, arg2):
+    # if argument is water and argument 2 is flour
+        # return doght
+    # else return
+        # not dough
+    return 'dough'
